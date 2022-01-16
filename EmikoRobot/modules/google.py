760a25122f8840561b77beb82531a83430359661
Ryaanbot <97766892+Ryaanbot@users.noreply.github.com>
2022-01-17 06:06:59 +0700

    Update google.py

--- EmikoRobot/modules/google.py
+++ EmikoRobot/modules/google.py
@@ -287,7 +287,7 @@ __help__ = """
 ❂ /github <username>*:* Get information about a GitHub user.
 ❂ /country <country name>*:* Gathering info about given country
 ❂ /imdb <Movie name>*:* Get full info about a movie with imdb.com
-❂ Emiko <query>*:* Emiko answers the query
+❂ GabutProtection <query>*:* GabutProtection answers the query
 
-  💡Ex: `Emiko where is Japan?`
+  💡Ex: `GabutProtection where is Japan?`
 """
