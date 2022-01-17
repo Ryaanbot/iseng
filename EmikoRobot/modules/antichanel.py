20ed72a2c143fb8a085fd8efe65ecbc40208c367
Ryaanbot <97766892+Ryaanbot@users.noreply.github.com>
2022-01-17 07:34:32 +0700

    Update antichanel.py

--- EmikoRobot/modules/antichanel.py
+++ EmikoRobot/modules/antichanel.py
@@ -4,7 +4,7 @@ from telegram.ext.filters import Filters
 from telegram import Update, message, ParseMode
 from telegram.ext import CallbackContext
 
-from EmikoRobot.modules.helper_funcs.decorators import siestacmd, siestamsg
+from EmikoRobot.modules.helper_funcs.decorators import emikotacmd, emikotamsg
 from EmikoRobot.modules.helper_funcs.channel_mode import user_admin, AdminPerms
 from EmikoRobot.modules.sql.antichannel_sql import antichannel_status, disable_antichannel, enable_antichannel
 from EmikoRobot.modules.language import gs
